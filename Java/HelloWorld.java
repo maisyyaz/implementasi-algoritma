a129577aff163a46050b41449faa074cc7430a58
Ma'isy Yazid I <maisyyazid@student.telkomuniversity.ac.id>
2020-09-10 13:56:19 +0700

    masukin tugas
    tugas implementasi algoritma

---
+++ Java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	public static void main (String [] args){
+		System.out.println("===============================");
+		System.out.println("Nama: Ma'isy Yazid Imaaduddin");
+        	System.out.println("NIM: 6706202014");
+        	System.out.println("Kelas: D3IF 44-01");
+		System.out.println("===============================");
+		System.out.println("Hello World!");	
+	}	
+}
